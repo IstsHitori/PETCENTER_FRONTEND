191f5b93fd0a01ce05927fdd10b88a04cef1bde0
Francisco Javier <seranojuliofranciscojavier@gmail.com>
2024-12-03 13:56:43 -0500

    Instalamos react-icons y estamos creando las interfaces para las historias clinicas

--- src/views/ViewHistories.tsx
+++ src/views/ViewHistories.tsx
@@ -1,5 +1,4 @@
 import { useVeterinarieStore } from "@/stores/useVeterinarieStore";
-import AccordionHistories from "@/components/histories/AccordionHistories";
 import { useEffect } from "react";
 export default function ViewHistories() {
   const patients = useVeterinarieStore((state) => state.patients);
@@ -11,10 +10,10 @@ export default function ViewHistories() {
   return (
     <>
       <section className="h-full">
+        <h1>Historias Clínicas</h1>
+        <p>Gestiona los registros médicos de tus pacientes</p>
         <div className="grid py-2 max-h-[530px] 2xl:max-h-[980px] overflow-auto 2xl:grid-cols-3 lg:grid-cols-2 px-2 gap-4 mt-2">
-          {patients.map((patient) => (
-            <AccordionHistories key={patient._id} patient={patient} />
-          ))}
+          {}
         </div>
       </section>
     </>
