012e4b6fa04ee81ef48425dfda66d34b9c4255de
Francisco Javier <seranojuliofranciscojavier@gmail.com>
2024-12-03 22:52:07 -0500

    Añadimos un combobox al ModalAddHistorie.tsx

--- src/components/histories/ModalAddHistorie.tsx
+++ src/components/histories/ModalAddHistorie.tsx
@@ -1,139 +1,61 @@
-import { Button, Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { useVeterinarieStore } from "@/stores/useVeterinarieStore";
-import { useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { AddHistory } from "@/types/HistoryTypes";
-import Error from "../Error";
+import { FormEvent } from "react";
+import { useState } from "react";
+import { GoPlus } from "react-icons/go";
+import { toast } from "react-toastify";
+import ComboSearchPatient from "./ComboSearchPatient";
 
 export default function ModalAddHistorie() {
-  const isModalAddHistorie = useVeterinarieStore(
-    (state) => state.isModalAddHistorie
-  );
-  const setModalAddHistorie = useVeterinarieStore(
-    (state) => state.setModalAddHistorie
-  );
-  const idPatient = useVeterinarieStore((state) => state.idPatient);
-  const searchPatientById = useVeterinarieStore(
-    (state) => state.searchPatientById
-  );
   const createHistory = useVeterinarieStore((state) => state.createHistory);
-  const patient = useVeterinarieStore((state) => state.patient);
+  const [objHistory, setObjHistory] = useState({
+    patient: "",
+    history: "",
+  });
   //--
-  useEffect(() => {
-    searchPatientById();
-  }, [idPatient]);
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<AddHistory>();
-
-  const { name, telephone, propietor, typePet } = patient;
-
-  const handleHistory = async (data: AddHistory) => {
-    const historyObject = { patient: patient._id, history: data.history };
-    await createHistory(historyObject);
-    reset();
+  const handleHistory = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(Object.values(objHistory).includes("")) {
+      toast.error("Error al registrar la historia, hay campos vacíos");
+      return;
+    }
   };
   return (
     <>
-      <Dialog
-        open={isModalAddHistorie}
-        as="div"
-        className="relative z-10 focus:outline-none"
-        onClose={() => {
-          setModalAddHistorie(false);
-          reset();
-        }}
-        __demoMode
-      >
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <form
-            onSubmit={handleSubmit(handleHistory)}
-            className=" flex min-h-full items-center justify-center p-4"
-          >
-            <DialogPanel
-              transition
-              className="w-full max-w-4xl rounded-xl  border p-6 bg-white duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
-            >
-              <section className="flex justify-between px-5 items-center gap-8">
-                <article className="bg-violet-500 w-full  rounded-md p-2 px-6 bg-cover">
-                  <DialogTitle
-                    as="h3"
-                    className="text-base/7 text-center font-semibold text-white mb-9"
-                  >
-                    Agrega una historia clínica al paciente
-                  </DialogTitle>
-
-                  <div className="mb-4 rounded-lg bg-violet-400 p-1 px-3 flex items-center justify-between">
-                    <div>
-                      <p className=" text-zinc-300 font-medium text-xs">
-                        Paciente
-                      </p>
-                      <p className="text-[15px] text-white">{name}</p>
-                    </div>
-                    <img src={"/typePet.svg"} width={16} />
-                  </div>
-
-                  <div className="mb-4 rounded-lg bg-violet-400 p-1 px-3 flex items-center justify-between">
-                    <div>
-                      <p className=" text-zinc-300 font-medium text-[12px]">
-                        Propietario
-                      </p>
-                      <p className="text-[15px] text-white">{propietor}</p>
-                    </div>
-                    <img src={"/person.svg"} width={16} />
-                  </div>
-
-                  <div className="mb-4 rounded-lg bg-violet-400 p-1 px-3 flex items-center justify-between">
-                    <div>
-                      <p className=" text-zinc-300 font-medium text-[12px]">
-                        Teléfono
-                      </p>
-                      <p className="text-[15px] text-white">{telephone}</p>
-                    </div>
-                    <img src={"/telephone.svg"} width={16} />
-                  </div>
-
-                  <div className="mb-4 rounded-lg bg-violet-400 p-1 px-3 flex items-center justify-between">
-                    <div>
-                      <p className=" text-zinc-300 font-medium text-[12px]">
-                        Tipo de animal
-                      </p>
-                      <p className="text-[15px] text-white">{typePet}</p>
-                    </div>
-                    <img src={"/typePet.svg"} width={16} />
-                  </div>
-
-                </article>
-
-                <article className=" flex flex-col items-center">
-                  <p className="mb-2">Escribe la nueva historia clínica</p>
-                  <textarea
-                    className=" border rounded-md h-[150px] outline-none p-2 w-[380px]"
-                    {...register("history", {
-                      required: "Este campo no puede ir vacío",
-                    })}
-                  ></textarea>
-                  {errors.history && (
-                    <Error message={errors.history.message as string} />
-                  )}
-                  <div className="mt-4">
-                    <Button
-                      className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
-                      type="submit"
-                      onClick={close}
-                    >
-                      Crear historia
-                    </Button>
-                  </div>
-                </article>
-              </section>
-            </DialogPanel>
+      <Dialog>
+        <DialogTrigger asChild>
+          <button className="flex items-center justify-center rounded-lg gap-x-4 p-2 hover:bg-black/80 transition-all bg-black text-white">
+            <GoPlus className="text-xl" />
+            <span className="text-sm md:text-md">Agregar historia</span>
+          </button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Agregar Nueva Historia Clínica</DialogTitle>
+            <DialogDescription>
+              Ingrese los detalles de la nueva historia clínica para el paciente
+              seleccionado.
+            </DialogDescription>
+          </DialogHeader>
+          <form onSubmit={handleHistory} className="space-y-4">
+            <div className="space-y-2">
+              <p>Buscar paciente</p>
+              <ComboSearchPatient /> 
+            </div>
+            <div className="space-y-2">
+              <label htmlFor="">Historia clínica</label>
+            </div>
+            <button className="flex items-center justify-center rounded-lg gap-x-4 p-2 px-3 text-sm hover:bg-black/85 transition-all bg-black text-white" type="submit">Agregar Historia</button>
           </form>
-        </div>
+        </DialogContent>
       </Dialog>
     </>
   );
--- src/views/ViewHistories.tsx
+++ src/views/ViewHistories.tsx
@@ -1,7 +1,7 @@
 import { useVeterinarieStore } from "@/stores/useVeterinarieStore";
 import { useEffect } from "react";
-import { GoPlus } from "react-icons/go";
 import ModalShowHistory from "@/components/histories/ModalShowHistory";
+import ModalAddHistorie from "@/components/histories/ModalAddHistorie";
 export default function ViewHistories() {
   const patients = useVeterinarieStore((state) => state.patients);
   const fetchPatients = useVeterinarieStore((state) => state.fetchPatients);
@@ -12,21 +12,20 @@ export default function ViewHistories() {
   return (
     <>
       <section className="h-full">
-        <div className="flex items-center  justify-between px-10 border-b py-4">
+        <div className="flex items-center flex-col md:flex-row md:space-y-0 space-y-3 justify-between md:px-10 border-b py-4">
           <div className="flex justify-center flex-col">
             <h1 className="text-2xl font-semibold">Historias Clínicas</h1>
-            <p className="text-gray-600 font-light">Gestiona los registros médicos de tus pacientes</p>
+            <p className="text-gray-600 font-light">
+              Gestiona los registros médicos de tus pacientes
+            </p>
           </div>
 
-          <button className="flex items-center justify-center rounded-lg gap-x-4 p-2 hover:bg-black/80 transition-all bg-black text-white">
-            <GoPlus className="text-xl" />
-            <span>Agregar historia</span>
-          </button>
+          <ModalAddHistorie />
         </div>
         <div className="grid py-2 max-h-[530px] 2xl:max-h-[980px] overflow-auto 2xl:grid-cols-4 lg:grid-cols-2 px-2 gap-4">
-          {
-            patients.map(patient => <ModalShowHistory key={patient._id} patient={patient} />)
-          }
+          {patients.map((patient) => (
+            <ModalShowHistory key={patient._id} patient={patient} />
+          ))}
         </div>
       </section>
     </>
