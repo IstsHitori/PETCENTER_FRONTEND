9c5ccefb32baa1d55eeb40a8016e85522cb130e9
Francisco Javier <seranojuliofranciscojavier@gmail.com>
2024-12-07 17:22:50 -0500

    Terminando de programar MainServices

--- src/components/services/MainServices.tsx
+++ src/components/services/MainServices.tsx
@@ -3,20 +3,33 @@ import { Input } from "../ui/input";
 import { useEffect, useState } from "react";
 import { useVeterinarieStore } from "@/stores/useVeterinarieStore";
 import ServiceDetails from "./ServiceDetails";
+import { Service } from "@/types/ServiceTypes";
 
 export default function MainServices() {
-  const servicesFetched = useVeterinarieStore((state) => state.services);
-  const fetchServices = useVeterinarieStore((state) => state.fetchServices);
+  const services = useVeterinarieStore((state) => state.services);
+
   const [filter, setFilter] = useState("todos");
-  const [services, setServices] = useState([...servicesFetched]);
+  const [servicesFilter, setServicesFilter] = useState([...services]);
+  const [searchPatient, setSearchPatient] = useState("");
+
+  const filterServices = (term: string, category: Service["state"]) => {
+    const filtered = services.filter(
+      (serv) =>
+        serv.patient.name.toLowerCase().includes(term) &&
+        (category === "todos" || serv.state === category) || serv.patient.propietor.toLowerCase().includes(term)
+    );
+    console.log(filtered)
+    setServicesFilter(filtered);
+  };
+  const handleCategoryChange = (category: Service["state"]) => {
+    setFilter(category);
+    filterServices(searchPatient,category);
+  }
 
   useEffect(() => {
-    const fetch = async () => {
-      await fetchServices();
-    };
-    fetch();
-  }, []);
-  console.log(services);
+    filterServices(searchPatient, filter);
+  }, [filter, searchPatient, setSearchPatient]);
+
   return (
     <div className="mt-4">
       <aside className="flex items-center justify-between">
@@ -24,35 +37,36 @@ export default function MainServices() {
           <Button
             variant={filter === "todos" ? "default" : "outline"}
             className="h-8"
-            onClick={() => setFilter("todos")}
+            onClick={() => handleCategoryChange("todos")}
           >
             Todos
           </Button>
           <Button
-            variant={filter === "pagados" ? "default" : "outline"}
+            variant={filter === "pagado" ? "default" : "outline"}
             className="h-8"
-            onClick={() => setFilter("pagados")}
+            onClick={() => handleCategoryChange("pagado")}
           >
             Pagados
           </Button>
 
           <Button
-            variant={filter === "cancelados" ? "default" : "outline"}
+            variant={filter === "no pagado" ? "default" : "outline"}
             className="h-8"
-            onClick={() => setFilter("cancelados")}
+            onClick={() => handleCategoryChange("no pagado")}
           >
-            Cancelados
+            No pagados
           </Button>
         </div>
         <Input
           className="max-w-[400px]"
           placeholder="Buscar por nombre de paciente o propietario"
+          onChange={(e) => setSearchPatient(e.target.value)}
         />
       </aside>
-      <section className="mt-2 grid grid-cols-3 gap-5 max-h-[800px] overflow-y-auto">
-        {servicesFetched.map((service) => (
+      <section className="mt-2 grid grid-cols-2 xl:grid-cols-3 gap-2 max-h-[400px] xl:max-h-[800px] overflow-y-auto">
+        {servicesFilter.map((service) => (
           <ServiceDetails key={service._id} service={service} />
-        ))} 
+        ))}
       </section>
     </div>
   );
--- src/views/ViewServices.tsx
+++ src/views/ViewServices.tsx
@@ -1,10 +1,20 @@
-import HeaderServices from "@/components/services/HeaderServices"
-import MainServices from "@/components/services/MainServices"
+import { useVeterinarieStore } from "@/stores/useVeterinarieStore";
+import HeaderServices from "@/components/services/HeaderServices";
+import MainServices from "@/components/services/MainServices";
+import { useEffect } from "react";
 export default function ViewServices() {
+  const fetchServices = useVeterinarieStore((state) => state.fetchServices);
+  useEffect(() => {
+    const fetch = async () => {
+      await fetchServices();
+    };
+    fetch();
+  }, []);
+
   return (
     <div className="py-4 px-12">
-        <HeaderServices />
-        <MainServices />
+      <HeaderServices />
+      <MainServices />
     </div>
-  )
+  );
 }
